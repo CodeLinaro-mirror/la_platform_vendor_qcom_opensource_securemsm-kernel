d11cf03a141030489428c1d1a93a53b6e2a152e2
Kuldeep Singh <quic_kuldsing@quicinc.com>
2025-03-17 15:39:32 +0530

    tz_log: Skip driver probe failure if qsee log not available
    qsee log plain buffer registration with QTEE can be a failure which
    further fails driver probing. Ideally, qsee log will not be collected
    but this should not limit tz_log driver to collect other
    logs(ex: tz/hyp/rm/tme etc.).
    
    Skip qsee_log node creation in case of failure and continue driver
    probing.
    
    Change-Id: I1c643351e7046a3355b9cd985011bfb6caa1178f
    Signed-off-by: Kuldeep Singh <quic_kuldsing@quicinc.com>

--- tz_log/tz_log.c
+++ tz_log/tz_log.c
@@ -1967,8 +1967,10 @@ static int tz_log_probe(struct platform_device *pdev)
 	 * access contents directly.
 	 */
 	ret = tzdbg_register_qsee_log_buf(pdev);
-	if (ret)
-		goto exit_free_diag_buf;
+	if (ret) {
+		pr_warn("Failure with plain qsee log buffer, Skipping qsee_log node creation..\n");
+		tzdbg.stat[TZDBG_QSEE_LOG].avail = false;
+	}
 
 	/* Allocate encrypted qsee and tz log buffer if encryption is enabled */
 	ret = tzdbg_allocate_encrypted_log_buf(pdev);
@@ -2003,7 +2005,6 @@ exit_free_encr_log_buf:
 	tzdbg_free_encrypted_log_buf(pdev);
 exit_free_qsee_log_buf:
 	tzdbg_free_qsee_log_buf(pdev);
-exit_free_diag_buf:
 	if (tzdbg.tz_qsee_plain_log_enabled)
 		kfree(tzdbg.diag_buf);
 	return -ENXIO;
