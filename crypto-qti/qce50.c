86d799882094ac6d8f7bdfeca2167c1df80962b6
Mohit Sharma <smohit@qti.qualcomm.com>
2025-07-19 22:42:23 -0700

    Revert "securemsm-kernel: qce: disable v1 WA on v2"
    This reverts commit 43745171f84e82f9d0912882e1110b5aaf3f7719.
    
    Change-Id: I16ef3f2b40128e9ab1bf5def12f1545e54fcc6df
    Signed-off-by: Mohit Sharma <smohit@qti.qualcomm.com>

--- crypto-qti/qce50.c
+++ crypto-qti/qce50.c
@@ -2279,7 +2279,7 @@ static int _qce_sps_add_data(struct qce_device *pce_dev,
 		if (len > SPS_MAX_PKT_SIZE) {
 			data_cnt = SPS_MAX_PKT_SIZE;
 		} else {
-			if (is_crypto_600(pce_dev) && out_pipe && pce_dev->fifo_eco_unavailable) {
+			if (is_crypto_600(pce_dev) && out_pipe) {
 				if ((sps_bam_pipe->iovec_count % 2) == 0) {
 					if (results_dump_enabled) {
 						data_cnt = len;
@@ -5359,7 +5359,7 @@ int qce_ablk_cipher_req(void *handle, struct qce_req *c_req)
 				SPS_IOVEC_FLAG_EOT|SPS_IOVEC_FLAG_NWD);
 
 	if (pce_dev->no_get_around) {
-		if (is_crypto_600(pce_dev) && pce_dev->fifo_eco_unavailable) {
+		if (is_crypto_600(pce_dev)) {
 			if ((get_desc_count(&pce_sps_data->in_transfer) % 2) == 0) {
 				// Add a dummy descriptor due to HW bug
 				rc = _qce_sps_add_cmd(pce_dev, 0,
