1052fb41e7e13d1e7aad54d324acb51a0dc95274
Nageswara reddy Karnati <quic_nkarnati@quicinc.com>
2025-09-01 04:51:42 -0700

    securemsm-kernel:QseecomComapt: Correct Size calculation of split bins
    Read size of each split segment and add it instead of calculating
    via 8th segment offset and size as that assumption was wrong in
    case of split bin.
    
    Change-Id: I48eba2b68d083c029417ad39ea48cdc52cc3e001
    Signed-off-by: Nageswara reddy Karnati <quic_nkarnati@quicinc.com>

--- smcinvoke/si_core_xts/qseecom.c
+++ smcinvoke/si_core_xts/qseecom.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/file.h>
@@ -339,8 +339,40 @@ char *firmware_request_from_smcinvoke(const char *appname, size_t *fw_size, stru
 		goto release_fw_entry00;
 	}
 
-	/*Total size of image will be the offset of last image + the size of last split image*/
-	*fw_size = fw_entrylast->size + offset[num_images-1];
+	/*Find the total image size*/
+	*fw_size = fw_entry00->size;
+	for (phi = 1; phi < num_images-1; phi++) {
+		snprintf(fw_name, ARRAY_SIZE(fw_name), "%s.b%02d", appname, phi);
+		rc = firmware_request_nowarn(&fw_entry, fw_name, smci_dev);
+		if (rc) {
+			pr_err("Failed to locate blob %s\n", fw_name);
+			goto release_fw_entrylast;
+		}
+
+		if (*fw_size > U32_MAX - fw_entry->size) {
+			release_firmware(fw_entry);
+			fw_entry = NULL;
+			goto release_fw_entrylast;
+		}
+
+		if ((*fw_size) < (offset[phi] + fw_entry->size))
+			*fw_size = offset[phi] + fw_entry->size;
+		release_firmware(fw_entry);
+		fw_entry = NULL;
+	}
+
+	if ((*fw_size) > U32_MAX - fw_entrylast->size)
+		goto release_fw_entrylast;
+
+
+	if (offset[phi] > U32_MAX - fw_entrylast->size)
+		goto release_fw_entrylast;
+
+
+	if ((*fw_size) < (offset[phi] + fw_entrylast->size))
+		*fw_size = offset[phi] + fw_entrylast->size;
+
+
 
 	/*Allocate memory for the buffer that will hold the split image*/
 	rc = qtee_shmbridge_allocate_shm((*fw_size), shm);
